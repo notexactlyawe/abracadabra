58993b668a7a6350eb5e73dacf83c058ff88d065
Cameron MacLeod <cmacleod170@googlemail.com>
2016-02-19 23:59:57 +0000

    Rework WaveHelper and add tests
    read_whole now module level instead of static
    now read_n_mili function
    wrappers for rewind, tell and setpos on the wave_read object
    add three tests for read_n_mili

--- Tests/test_wavehelper.py
+++ Tests/test_wavehelper.py
@@ -1,14 +1,37 @@
-from Tools.wavehelper import WaveHelper
+import Tools.wavehelper as wvhelp
+import wave
+
+filename = 'Samples/test.wav'
 
 def test_read_whole_len():
-    import wave
-
-    filename = 'Samples/test.wav'
-
     wav_r = wave.open(filename, 'r')
     expected_len = wav_r.getnframes()
-    actual_len = len(WaveHelper.read_whole(filename))
+    wav_r.close()
+    actual_len = len(wvhelp.read_whole(filename))
     print "{0} == {1}".format(expected_len, actual_len)
     assert expected_len == actual_len
 
+def test_read_n_mili_len():
+    wav_r = wave.open(filename, 'r')
+    t_p_s = (1.0 / wav_r.getframerate()) * 1000
+    s_t_g = int(16 / t_p_s)
+    wav_r.close()
+
+    w = wvhelp.WaveHelper(filename, True)
+    ms_samples = w.read_n_mili(16)
+    print ms_samples
+    print "exp: {0}, act {1}".format(s_t_g, len(ms_samples))
+    assert s_t_g == len(ms_samples)
+
+def test_read_n_mili_end_file():
+    w = wvhelp.WaveHelper(filename, True)
+    w.seek(w.wav.getnframes() - 1)
+    samples = w.read_n_mili(16)
+    assert len(samples) == 1
+
+def test_read_n_mili_type():
+    w = wvhelp.WaveHelper(filename, True)
+    samples = w.read_n_mili(16)
+    assert type(samples) is list
+    assert type(samples[0]) is int
 # Maybe figure out how to compare graphs?
--- Tools/wavehelper.py
+++ Tools/wavehelper.py
@@ -1,12 +1,46 @@
 import wave
 import struct
 
+def read_whole(filename):
+    wav_r = wave.open(filename, 'r')
+    ret = []
+    while wav_r.tell() < wav_r.getnframes():
+        decoded = struct.unpack("<h", wav_r.readframes(1))
+        ret.append(decoded)
+    return ret
+
 class WaveHelper():
-    @staticmethod
-    def read_whole(filename):
-        wav_r = wave.open(filename, 'r')
+    def __init__(self, filename, read):
+        mode = 'r' if read else 'w'
+        self.wav = wave.open(filename, mode)
+
+    def read_n_mili(self, n):
         ret = []
-        while wav_r.tell() < wav_r.getnframes():
-            decoded = struct.unpack("<h", wav_r.readframes(1))
-            ret.append(decoded)
+
+        time_per_sample = (1.0 / self.wav.getframerate()) * 1000
+        samples_to_get  = int(n / time_per_sample)
+
+        if samples_to_get == 0:
+            samples_to_get = 1
+
+        for _ in range(samples_to_get):
+            try:
+                decoded = struct.unpack("<h", self.wav.readframes(1))
+            except:
+                # if samples left < samples_to_get
+                # ie readframes(1) returns ''
+                break
+            if len(decoded) == 2:
+                ret.append((decoded[0] + decoded[1]) / 2)
+            else:
+                ret.append(decoded[0])
         return ret
+
+    def rewind(self):
+        self.wav.rewind()
+
+    def pos(self):
+        return self.wav.tell()
+
+    def seek(self, sample_no):
+        self.wav.setpos(sample_no)
