b948cb2a23c6f28a2aa3b34138e428a8481063ef
Cameron MacLeod <cmacleod170@googlemail.com>
2016-02-19 19:24:58 +0000

    Better define only test in test_fingerprint

--- Tests/test_fingerprint.py
+++ Tests/test_fingerprint.py
@@ -1,6 +1,5 @@
 from Classifier.fingerprint import Fingerprint
 from nose.tools import assert_raises
 
-def test_init():
+def test_init_no_args():
     assert_raises(ValueError, Fingerprint)
-    f = Fingerprint(filename='Samples\\file3.wav')
