c7dcbf420dc0f6ab7d5f056496fdcb4fde675d27
Cameron MacLeod <cmacleod170@gmail.com>
2016-05-23 14:42:53 +0100

    Remove now obsolete code
    fingerprint.py has been converted into a boilerplate file
    All tests removed
    ASCII graphing files removed

--- Fingerprint/fingerprint.py
+++ Fingerprint/fingerprint.py
@@ -1,21 +1,12 @@
-from __future__ import division
 from Tools.wavehelper import WaveHelper
 import Tools.misc as misc
 import numpy as np
 import matplotlib.pyplot as plt
 import logging
 
-class Fingerprint():
-    def __init__(self, filename=None, rec_stream=None):
-        if filename is None and rec_stream is None:
-            raise ValueError('No filename or stream provided')
-        if filename is None and rec_stream is not None:
-            raise NotImplementedError('Sorry guys')
-        self.wav_r = WaveHelper(filename, read=True, debug=True)
-        self.samples = self.wav_r.read_whole()
-        self.mili = 16
-        self.freq_arr = np.linspace(0, 22050, self.wav_r.samples_per_n_mili(self.mili)/2)
-        logging.debug("Fingerprint initialised")
+class Fingerprint:
+    def __init__(self):
+        pass
 
     def windows(self, l, n):
         chunk_size = self.wav_r.samples_per_n_mili(n)
@@ -23,19 +14,8 @@ class Fingerprint():
             yield chunk
 
     def fingerprint(self):
-        no = 1
-        total_len = 0
-        fprint = []
-        for window in self.windows(self.samples, self.mili):
-            f = misc.fourier(window)
-            temp = []
-            for num, seg in enumerate(misc.chunks(f, len(f)//16)):
-                f_idx = np.where(seg==max(seg))[0][0] + num * len(f)//16
-                freq = self.freq_arr[f_idx]
-                temp.append(freq)
-            fprint.append(temp)
-        return fprint
-    
+        pass
+
 if __name__ == "__main__":
     f = Fingerprint(filename="Samples/336739__astronautchild__goddog.wav")
     f.fingerprint()
--- Tests/test_fingerprint.py
+++
@@ -1,5 +0,0 @@
-from Classifier.fingerprint import Fingerprint
-from nose.tools import assert_raises
-
-def test_init_no_args():
-    assert_raises(ValueError, Fingerprint)
